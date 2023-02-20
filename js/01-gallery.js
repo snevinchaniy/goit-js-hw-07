9a83d86bd14fd909fae97083e15d6f40ed4ffbc1
snevinchaniy <85674052+snevinchaniy@users.noreply.github.com>
2023-02-20 12:39:09 +0200

    added completed tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Створення розмітки
+
+const gallery = document.querySelector('.gallery');
+
+const createGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class = 'gallery__item'>
+      <a class = 'gallery__link' href = '${original}'>
+      <img class = 'gallery__image' src = '${preview}' data-source = '${original}' alt = '${description}'>
+      </a></div>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', createGallery);
+
+// Відкриття модального вікна по кліку на елементі галереї
+
+const clickOnImage = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const imageSelect = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+      <img src='${imageSelect}'>
+  `,
+    {
+      onShow: () => {
+        document.addEventListener('keydown', close);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', close);
+      },
+    }
+  );
+
+  instance.show();
+
+  // Закриття з клавіатури
+
+  gallery.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+};
+
+gallery.addEventListener('click', clickOnImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Створення розмітки
+
+const gallery = document.querySelector('.gallery');
+
+const createGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class = 'gallery__item' href = '${original}'>
+      <img class = 'gallery__image' src = '${preview}' alt = '${description}'>
+      </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', createGallery);
+
+// Ініціалізація бібліотеки
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
